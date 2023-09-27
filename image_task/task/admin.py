b69ef3266492146d055d4f23b8dc3bfdbb000058
Grzegorz-Oledzki <grzeoledzki@gmail.com>
2023-09-27 19:22:36 +0200

    add admin view

--- image_task/task/admin.py
+++ image_task/task/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from task.models import User, Image
+
+
+@admin.register(User)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "tier")
+
+
+@admin.register(Image)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["name"]
